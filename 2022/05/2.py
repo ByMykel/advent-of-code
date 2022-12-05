0def6efdef22238fc3d41f19a4c9d9826e65f6ed
Mihail Dragnev <bymykel5@gmail.com>
2022-12-05 09:40:46 +0100

    Day 5 of 2022.

---
+++ 2022/05/2.py
@@ -0,0 +1,31 @@
+import re
+
+file = open("input.txt").read()
+
+out = 0
+readingStack = True
+stack = [[] for i in range(9)]
+
+for i in file.split("\n"):
+    if readingStack and '[' in i:
+        for j in range(0, len(i), 4):
+            if i[j + 1] != ' ':
+                stack[j // 4].append(i[j + 1])
+        continue
+    if (i == "" or '[' not in i) and readingStack:
+        for j in range(9):
+            stack[j] = stack[j][::-1]
+        readingStack = False
+        continue
+
+    move = re.findall(r'\d+', i)
+
+    if move == []:
+        continue
+
+    items = [stack[int(move[1]) - 1].pop() for j in range(int(move[0]))]
+    stack[int(move[2]) - 1].extend(items[::-1])
+
+out = ''.join([j[-1] for j in stack])
+
+print(out)
